cb6f20837149dfa42dc15e29ce8d05f3e2e90568
Alireza Savand <alireza.savand@gmail.com>
2014-10-06 00:11:07 +0400

    Initial TestSanitizeHTML

--- tests/test_sanitize_html.py
+++ tests/test_sanitize_html.py
@@ -1,3 +1,21 @@
+from unittest import TestCase
+
 import sanitize
 
 
+class TestSanitizeHTML(TestCase):
+    def _html(self, html_source, expected_data, base_uri=None, add_nofollow=False):
+        """
+        :type html_source: str
+        :type expected_data: str
+        :type base_uri: str
+        :type add_nofollow: bool
+        """
+        self.assertEqual(
+            sanitize.HTML(
+                htmlSource=html_source,
+                baseuri=base_uri,
+                addnofollow=add_nofollow
+            ),
+            expected_data
+        )
